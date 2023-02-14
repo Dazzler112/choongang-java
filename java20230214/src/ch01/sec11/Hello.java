48b26e631dce85c4f789b3727969f58e9ffce024
Dazzler112 <108716970+Dazzler112@users.noreply.github.com>
2023-02-14 10:54:25 +0900

    2022.02.14 자바 수업 깃허브 테스트

---
+++ java20230214/src/ch01/sec11/Hello.java
@@ -0,0 +1,28 @@
+package ch01.sec11; //주석에 대한 설명
+/**
+ * @author 여러분의 이름
+ */
+/*
+ 장제목: 1장 자바 시작하기
+ 작성일: 2022.08.24
+ */
+public class Hello {
+	//프로그램 실행 진입점
+	public static void main(String[] args) {
+		//<--한줄주석
+		
+		/*
+		 *<--- 여러줄 주석 
+		 * 
+		 */
+		
+		// <-- 단축키 ctrl + /
+		
+//		여러줄  <-여러줄 하는법 드래그 하고 ctrl+/
+//		여러줄
+//		여러줄
+		
+		//콘솔에 출력하는 실행문
+		System.out.println("Hello, Java");
+	}
+}
---
+++ java20230214/src/ch01/sec11/MainShortCut.java
@@ -0,0 +1,17 @@
+package ch01.sec11;
+
+public class MainShortCut {//main 타이핑후 ctrl +spacebar 맨위에 main메소드 클릭
+	public static void main(String[] args) {
+		System.out.println("Hi JAVA");
+		System.out.println("출력 되는 내용");
+		System.out.println("출력!!");
+		//sysout 타이핑후 ctrl + spacebar 한후 클릭
+		
+		//주석 ctrl +/
+		//실행 ctrl+f11
+		//창크기 crtl + m
+		//한줄 지우기 ctrl+d
+		//줄 옮기기 alt+방향키 위 아래
+		//힌트 얻기 crtl + space
+	}
+}
